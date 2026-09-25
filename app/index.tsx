6f4e3b176b4d22c2a5fca796bdcca9d893ff2d42
Habib Akhtar <habibakhtar593@gmail.com>
2026-09-25 18:39:43 +0100

    connection made to tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,8 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200">bensan</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
